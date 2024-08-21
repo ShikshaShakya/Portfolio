77c342890cc2417bbc5c7528e98d16dcdcf02553
ShikshaShakya <shikshashakya220@gmail.com>
2024-08-22 00:30:28 +0530

    adding

---
+++ Data/index.ts
@@ -0,0 +1,145 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Experience", link: "#experience" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-1 lg:row-span-1 md:col-span-6 md:row-span-4",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 lg:row-span-1 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 5,
+    title: "I love developing sleek & modern websites",
+    description: "Just between you and me",
+    className: "lg:col-span-3 lg:row-span-1 md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 lg:row-span-1 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-1 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "AIVault",
+    des: "Developed a modern looking web page for an AI chatting company AIVault with sleek parallax effects and bento box layouts.",
+    img: "/p1.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://aivault.onrender.com/",
+    available: "https://aivault.onrender.com/",
+  },
+  {
+    id: 2,
+    title: "Knowledge Sharing Platform for NRSC",
+    des: "Developed a platform for NRSC to publish blog posts, articles, and research papers, showcasing their learnings and achievements.",
+    img: "/p2.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/projects",
+    available: "This app is not live due the to privacy concerns of NRSC.",
+  },
+  // {
+  //   id: 3,
+  //   title: "AI Image SaaS - Canva Application",
+  //   des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+  //   img: "/p3.svg",
+  //   iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+  //   link: "https://github.com/adrianhajdin/ai_saas_app",
+  // },
+  // {
+  //   id: 4,
+  //   title: "Animated Apple Iphone 3D Website",
+  //   des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+  //   img: "/p4.svg",
+  //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+  //   link: "https://github.com/adrianhajdin/iphone",
+  // },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Full Stack Intern at NRSC, ISRO",
+    desc: "Developed a web-based platform using React.js, to publish blog posts, articles, and research papers, showcasing their learnings and achievements.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Freelance Web App Dev Project",
+    desc: "Designed and developed the frontend of a web app for a client using React.js.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  //   {
+  //     id: 3,
+  //     title: "Freelance App Dev Project",
+  //     desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+  //     className: "md:col-span-2", // change to md:col-span-2
+  //     thumbnail: "/exp3.svg",
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "Lead Frontend Developer",
+  //     desc: "Developed and maintained user-facing features using modern frontend technologies.",
+  //     className: "md:col-span-2",
+  //     thumbnail: "/exp4.svg",
+  //   },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    href: "https://github.com/ShikshaShakya/",
+  },
+  {
+    id: 2,
+    img: "/link.svg",
+    href: "https://www.linkedin.com/in/shiksha-shakya-b06ba4187/",
+  },
+  // {
+  //   id: 3,
+  //   img: "/link.svg",
+  // },
+];
